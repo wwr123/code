7883ed663df6d4d059776936cfdc1385e5830f65
wwr123 <1803489036@qq.com>
2017-07-21 20:41:10 +0800

    用队列实现

---
+++ 剑指offer/层序遍历二叉树/test.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> PrintFromTopToBottom(TreeNode* root) {
+        vector<int> v;
+        if(root==NULL)
+            return v;
+    	queue<TreeNode*> q;
+    	q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* front=q.front();
+            q.pop();
+            v.push_back(front->val);
+            if(front->left)
+                q.push(front->left);
+            if(front->right)
+                q.push(front->right);
+        }
+        return v;
+
+    }
+};
